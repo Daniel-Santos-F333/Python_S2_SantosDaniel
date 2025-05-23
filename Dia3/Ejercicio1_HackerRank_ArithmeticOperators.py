4eed8e9474870497acf8d94c7881687034b8d022
DanielS <cuentaanormal1208@gmail.com>
2025-05-23 12:38:32 -0500

    Feat: :sparkles: Folders added and new files

---
+++ Dia3/Ejercicio1_HackerRank_ArithmeticOperators.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    suma = a + b
+    resta = a - b
+    multip = a * b
+    print(suma)
+    print(resta)
+    print(multip)
---
+++ Dia3/Ejercicio2_HackerRank_PythonDivision.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    integerDiv = a//b
+    floatDiv = a/b
+    
+    print(integerDiv)
+    print(floatDiv)
+    
